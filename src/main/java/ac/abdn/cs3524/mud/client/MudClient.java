98e00adfd9a2df20900103b52d7c8d7bb332eecf
WojtekWilk <47253851+WojtekWilk@users.noreply.github.com>
2021-04-21 10:38:01 +0100

    Made Skeleton Menu for the Client

--- src/main/java/ac/abdn/cs3524/mud/client/MudClient.java
+++ src/main/java/ac/abdn/cs3524/mud/client/MudClient.java
@@ -20,6 +20,60 @@ public class MudClient {
             Config clientConfig = new Config("content/client.properties");    // Client config
             String serverHostName = clientConfig.getProperty("server.hostname").orElse("localhost");
             int serverPort = Integer.parseInt(clientConfig.getProperty("server.port").orElse("8080"));
+            
+            // Main Menu for the Client
+            MainMenu: {
+                while (true) {
+                    System.out.println("Choose one of the options (by inputting its number then pressing ENTER):");
+                    System.out.println("1. Create a new game");
+                    System.out.println("2. Join existing game");
+                    System.out.println("3. View existing games");
+
+                    Scanner in1 = new Scanner(System.in);
+                    //in.useDelimiter("\n"); // not sure if needed
+                    String menuInputOne;
+                    menuInputOne = in1.next();
+
+                    if (menuInputOne.equalsIgnoreCase("1")) {
+                        System.out.println("Creating New Game");
+                        // WIP
+                        // Will use the code in the following lines
+                        // Now just breaks and continues
+                        // Technical functional
+                        break;
+                    } else if (menuInputOne.equalsIgnoreCase("2")) {
+                        while (true) {
+
+                            System.out.println("Input game's IP you want to join or press ENTER to go back");
+                            Scanner in2 = new Scanner(System.in);
+                            //in2.useDelimiter("\n"); // not sure needed
+                            String menuInputTwo;
+                            menuInputTwo = in2.nextLine();
+
+                            if (menuInputTwo.isEmpty()) {
+                                break;
+                            } else if (menuInputTwo.equalsIgnoreCase("1111")) { // "1111" being example ip
+                                System.out.println("Joining: " + menuInputTwo);
+                                // WIP
+                                // Will join a game from available IPs
+                                // Now just breaks MainMenu and continues
+                                // Not functional
+                                break MainMenu;
+                            } else {
+                                System.out.println("Invalid input, please try again");
+                            }
+                        }
+                    } else if (menuInputOne.equalsIgnoreCase("3")) {
+                        System.out.println("List of available games:");
+                        // WIP
+                        // Will print out a list of available games by IP
+                        // Now does nothing
+                        // Not functional
+                    } else {
+                        System.out.println("Invalid input, please try again");
+                    }
+                }
+            }
 
             // Connect to remote server
             Registry registry = LocateRegistry.getRegistry(serverHostName,serverPort);
