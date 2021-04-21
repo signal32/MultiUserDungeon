a90081682323cb30439206fff7ba1997f31cb265
WojtekWilk <47253851+WojtekWilk@users.noreply.github.com>
2021-04-21 13:18:16 +0100

    Almost functional, but not quite Menu

--- src/main/java/ac/abdn/cs3524/mud/client/MudClient.java
+++ src/main/java/ac/abdn/cs3524/mud/client/MudClient.java
@@ -10,6 +10,7 @@ import org.slf4j.LoggerFactory;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.util.Scanner;
+import java.util.UUID;
 
 public class MudClient {
 
@@ -22,6 +23,10 @@ public class MudClient {
             String serverHostName = clientConfig.getProperty("server.hostname").orElse("localhost");
             int serverPort = Integer.parseInt(clientConfig.getProperty("server.port").orElse("8080"));
             
+            // Connect to remote server
+            Registry registry = LocateRegistry.getRegistry(serverHostName,serverPort);
+            MudServerInterface server = (MudServerInterface) registry.lookup("MudServerInterface");
+            
             // Main Menu for the Client
             MainMenu: {
                 while (true) {
@@ -29,63 +34,59 @@ public class MudClient {
                     System.out.println("1. Create a new game");
                     System.out.println("2. Join existing game");
                     System.out.println("3. View existing games");
-
                     Scanner in1 = new Scanner(System.in);
-                    //in.useDelimiter("\n"); // not sure if needed
                     String menuInputOne;
                     menuInputOne = in1.next();
 
                     if (menuInputOne.equalsIgnoreCase("1")) {
                         System.out.println("Creating New Game");
-                        // WIP
-                        // Will use the code in the following lines
-                        // Now just breaks and continues
-                        // Technical functional
+                        GameInterface game = server.newGame("myMud");
+                        LOGGER.info("Player count: {}",game.playerCount());
+                        PlayerInterface player = server.joinGame(game.getID(), "test player");
                         break;
                     } else if (menuInputOne.equalsIgnoreCase("2")) {
                         while (true) {
-
                             System.out.println("Input game's IP you want to join or press ENTER to go back");
                             Scanner in2 = new Scanner(System.in);
-                            //in2.useDelimiter("\n"); // not sure needed
                             String menuInputTwo;
                             menuInputTwo = in2.nextLine();
-
                             if (menuInputTwo.isEmpty()) {
                                 break;
-                            } else if (menuInputTwo.equalsIgnoreCase("1111")) { // "1111" being example ip
-                                System.out.println("Joining: " + menuInputTwo);
-                                // WIP
-                                // Will join a game from available IPs
-                                // Now just breaks MainMenu and continues
-                                // Not functional
-                                break MainMenu;
                             } else {
-                                System.out.println("Invalid input, please try again");
+                                if (isUUID(menuInputTwo)) {
+                                    if (server.listGames().contains(UUID.fromString(menuInputTwo))){
+                                    System.out.println("Joining: " + menuInputTwo);
+                                    GameInterface game = server.getGame(UUID.fromString(menuInputTwo));
+                                    LOGGER.info("Player count: {}",game.playerCount());
+                                    PlayerInterface player = server.joinGame(UUID.fromString(menuInputTwo), "test player");
+                                    break MainMenu;
+                                    }
+                                } else {
+                                    System.out.println("Invalid input, please try again");
+                                }
                             }
                         }
                     } else if (menuInputOne.equalsIgnoreCase("3")) {
-                        System.out.println("List of available games:");
-                        // WIP
-                        // Will print out a list of available games by IP
-                        // Now does nothing
-                        // Not functional
+                        if (server.listGames().size() == 0) {
+                            System.out.println("Currently, there are no available games");
+                        } else {
+                            System.out.println("List of available games:");
+                            for (int i = 0; i < server.listGames().size(); i++) {
+                                System.out.println((i + 1) + ". " + server.listGames().get(i).toString());
+                            }
+                        }
                     } else {
                         System.out.println("Invalid input, please try again");
                     }
                 }
             }
 
-            // Connect to remote server
-            Registry registry = LocateRegistry.getRegistry(serverHostName,serverPort);
-            MudServerInterface server = (MudServerInterface) registry.lookup("MudServerInterface");
-
             // Start game
-            GameInterface game = server.newGame("myMud");
-            LOGGER.info("Player count: {}",game.playerCount());
+            //GameInterface game = server.newGame("myMud");
+            //LOGGER.info("Player count: {}",game.playerCount());
 
             // Join Game
-            PlayerInterface player = server.joinGame(game.getID(), "test player");
+            //PlayerInterface player = server.joinGame(game.getID(), "test player");
             LOGGER.info("Added '{}' to game '{}' at '{}'", player.getName(), game.getID(), player.getLocation());
 
             LOGGER.info("Player count: {}",game.playerCount());
@@ -107,6 +108,15 @@ public class MudClient {
         }
         catch (Exception e){
             LOGGER.error("Fatal error: {}", e.getMessage());
+        }
+    }
+    
+    static boolean isUUID(String string) {
+        try {
+            UUID.fromString(string);
+            return true;
+        } catch (Exception e) {
+            return false;
         }
     }
 }
