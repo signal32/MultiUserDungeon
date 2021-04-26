fbd017a2a21122c7ad1517df4b349ca01400b81a
signal32 <55558643+signal32@users.noreply.github.com>
2021-04-26 14:41:00 +0100

    Improved Client structure

--- src/main/java/ac/abdn/cs3524/mud/client/MudClient.java
+++ src/main/java/ac/abdn/cs3524/mud/client/MudClient.java
@@ -7,6 +7,7 @@ import ac.abdn.cs3524.mud.server.MudServerInterface;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.util.Scanner;
@@ -16,124 +17,120 @@ public class MudClient {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(MudClient.class);
 
+    private PlayerInterface player;
+    private GameInterface game;
+    private MudServerInterface server;
+    private boolean run = true;
+
     public static void main(String[] args) {
-        try{
+        try {
             // Setup and get config
             Config clientConfig = new Config("content/client.properties");    // Client config
             String serverHostName = clientConfig.getProperty("server.hostname").orElse("localhost");
             int serverPort = Integer.parseInt(clientConfig.getProperty("server.port").orElse("8080"));
-            
+
+            MudClient app = new MudClient();
+
             // Connect to remote server
-            Registry registry = LocateRegistry.getRegistry(serverHostName,serverPort);
-            MudServerInterface server = (MudServerInterface) registry.lookup("MudServerInterface");
+            Registry registry = LocateRegistry.getRegistry(serverHostName, serverPort);
+            app.server = (MudServerInterface) registry.lookup("MudServerInterface");
 
-            // Allocate a player
-            PlayerInterface player = null;
-            
-            // Allocate a game
-            GameInterface game;
-            
-            // Main Menu for the Client
-            MainMenu: {
+            // Run the game
+            while (app.run) {
+                app.menu();
+                app.game();
+            }
+
+            // Close down game
+            app.exit();
+
+        } catch(Exception e){
+                LOGGER.error("Fatal error: {}", e.getMessage());
+        }
+    }
+
+
+    private void menu() throws RemoteException {
+        boolean menu = true;
+        while (menu) {
+            System.out.println("Choose one of the options (by inputting its number then pressing ENTER):");
+            System.out.println("1. Create a new game");
+            System.out.println("2. Join existing game");
+            System.out.println("3. View existing games");
+            System.out.println("4. Exit the server");
+            Scanner in1 = new Scanner(System.in);
+            String menuInputOne;
+            menuInputOne = in1.next();
+
+            if (menuInputOne.equalsIgnoreCase("1")) {
+                System.out.println("Creating New Game");
+                game = server.newGame("myMud");
+                LOGGER.info("Player count: {}", game.playerCount());
+                player = server.joinGame(game.getID(), "test player");
+
+            } else if (menuInputOne.equalsIgnoreCase("2")) {
                 while (true) {
-                    System.out.println("Choose one of the options (by inputting its number then pressing ENTER):");
-                    System.out.println("1. Create a new game");
-                    System.out.println("2. Join existing game");
-                    System.out.println("3. View existing games");
-                    System.out.println("4. Exit the server");
-                    Scanner in1 = new Scanner(System.in);
-                    String menuInputOne;
-                    menuInputOne = in1.next();
-
-                    if (menuInputOne.equalsIgnoreCase("1")) {
-                        System.out.println("Creating New Game");
-                        game = server.newGame("myMud");
-                        LOGGER.info("Player count: {}",game.playerCount());
-                        player = server.joinGame(game.getID(), "test player");
+                    System.out.println("Input game's IP you want to join or press ENTER to go back");
+                    Scanner in2 = new Scanner(System.in);
+                    String menuInputTwo;
+                    menuInputTwo = in2.nextLine();
+                    if (menuInputTwo.isEmpty()) {
                         break;
-                    } else if (menuInputOne.equalsIgnoreCase("2")) {
-                        while (true) {
-                            System.out.println("Input game's IP you want to join or press ENTER to go back");
-                            Scanner in2 = new Scanner(System.in);
-                            String menuInputTwo;
-                            menuInputTwo = in2.nextLine();
-                            if (menuInputTwo.isEmpty()) {
-                                break;
-                            } else {
-                                if (isUUID(menuInputTwo)) {
-                                    if (server.listGames().contains(UUID.fromString(menuInputTwo))){
-                                    System.out.println("Joining: " + menuInputTwo);
-                                    game = server.getGame(UUID.fromString(menuInputTwo));
-                                    LOGGER.info("Player count: {}",game.playerCount());
-                                    player = server.joinGame(UUID.fromString(menuInputTwo), "test player");
-                                    break MainMenu;
-                                    }
-                                } else {
-                                    System.out.println("Invalid input, please try again");
-                                }
-                            }
-                        }
-                    } else if (menuInputOne.equalsIgnoreCase("3")) {
-                        if (server.listGames().size() == 0) {
-                            System.out.println("Currently, there are no available games");
-                        } else {
-                            System.out.println("List of available games:");
-                            for (int i = 0; i < server.listGames().size(); i++) {
-                                System.out.println((i + 1) + ". " + server.listGames().get(i).toString());
-                            }
-                        }
-                    } else if (menuInputOne.equalsIgnoreCase("4")) {
-                        System.out.println("Are you sure about this? (y/n)");
-                        Scanner in3 = new Scanner(System.in);
-                        String menuInputThree;
-                        menuInputThree = in3.next();
-                        if (menuInputThree.equalsIgnoreCase("y")) return;
                     } else {
-                        System.out.println("Invalid input, please try again");
+                        if (isUUID(menuInputTwo)) {
+                            if (server.listGames().contains(UUID.fromString(menuInputTwo))) {
+                                System.out.println("Joining: " + menuInputTwo);
+                                game = server.getGame(UUID.fromString(menuInputTwo));
+                                LOGGER.info("Player count: {}", game.playerCount());
+                                player = server.joinGame(UUID.fromString(menuInputTwo), "test player");
+                                return;
+                            }
+                        } else {
+                            System.out.println("Invalid input, please try again");
+                        }
                     }
                 }
+            } else if (menuInputOne.equalsIgnoreCase("3")) {
+                if (server.listGames().isEmpty()) {
+                    System.out.println("Currently, there are no available games");
+                } else {
+                    System.out.println("List of available games:");
+                    for (int i = 0; i < server.listGames().size(); i++) {
+                        System.out.println((i + 1) + ". " + server.listGames().get(i).toString());
+                    }
+                }
+            } else if (menuInputOne.equalsIgnoreCase("4")) {
+                System.out.println("Are you sure about this? (y/n)");
+                Scanner in3 = new Scanner(System.in);
+                String menuInputThree;
+                menuInputThree = in3.next();
+                if (menuInputThree.equalsIgnoreCase("y")) {run = false; return;}
+            } else {
+                System.out.println("Invalid input, please try again");
             }
-
-            if (player == null) {
-                LOGGER.error("Player is null");
-                System.exit(-1);
-            }
-
-            // Start game
-            //GameInterface game = server.newGame("myMud");
-            //LOGGER.info("Player count: {}",game.playerCount());
-
-            // Join Game
-            //PlayerInterface player = server.joinGame(game.getID(), "test player");
-           /*LOGGER.info("Added '{}' to game '{}' at '{}'", player.getName(), game.getID(), player.getLocation());
-
-            LOGGER.info("Player count: {}",game.playerCount());*/
-
-            player.move("east");
-            LOGGER.info("Location {}",player.getLocation());
-
-            // Main Game Loop
-            boolean run = true;
-            while (run){
-                // TODO wait for input
-                // TODO update
-                // TODO display
-                
-                // if command == exit, then:
-                run = false;
-            }
-
-            // TODO leave game
-            server.leaveGame(game,player);
-            LOGGER.info("Location {}",player.getLocation());
-            // TODO close connections
         }
-        catch (Exception e){
-            LOGGER.error("Fatal error: {}", e.getMessage());
-        }
-        main(null);
     }
-    
+
+    private void game() throws RemoteException{
+        System.out.println("Hello" + player.getName() +", You have entered the game at: " + player.getLocation() + player.getLocationInfo());
+        boolean play = true;
+        while (play) {
+            player.getLocationInfo();
+            // TODO wait for input
+            // TODO update
+            // TODO display
+
+            // if command == exit, then:
+            play=false;
+        }
+    }
+
+    private void exit() throws RemoteException {
+        server.leaveGame(game,player);
+    }
+
+
+
     static boolean isUUID(String string) {
         try {
             UUID.fromString(string);
